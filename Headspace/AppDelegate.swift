c7fe7fe9d25a84cce8f4ba37986d4a2e71c04888
Jasper Kuperus <bitbucket@jasperkuperus.nl>
2018-06-17 14:17:04 +0200

    Initial Commit

---
+++ Headspace/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Headspace
+//
+//  Created by Jasper Kuperus on 17/06/2018.
+//  Copyright © 2018 Jasper Kuperus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
