a07f556ed40213f4d73a05d20e422f7bd353c866
Jasper Kuperus <bitbucket@jasperkuperus.nl>
2018-06-17 14:35:57 +0200

    Menu bar application with logo and quit

--- Headspace/AppDelegate.swift
+++ Headspace/AppDelegate.swift
@@ -11,11 +11,20 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem =  NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
-
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.menu = statusMenu
+        let icon = NSImage(named: NSImage.Name("statusIcon"))
+//        icon?.isTemplate = true
+        statusItem.image = icon
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
